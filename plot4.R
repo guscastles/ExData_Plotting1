ac2ad7b89f6e6a0fdb3fca0347df6584cb143139
Gus Castles <guscastles@gmail.com>
2018-10-27 21:47:18 +1100

    Add stubs for plot functions

--- plot4.R
+++ plot4.R
@@ -1,11 +1,20 @@
 source("plot2.R")
 source("plot3.R")
 
-
 plot4 <- function(dataset) {
         par(mfcol = c(2, 2))
         plot2(dataset, ylabel = "Global Active Power")
         plot3(dataset)
+        plot4a(dataset)
+        plot4b(dataset)
+}
+
+plot4a <- function(dataset) {
+        
+}
+
+plot4b <- function(dataset) {
+        
 }
 
 make_plot4 <- function() {
